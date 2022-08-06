1dcf94129a0024751cc0d6dcabd2b56f2eae51ce
psd9988 <psd9988@gmail.com>
2022-08-06 11:19:32 +0530

    styling

--- Assignment 3 Calculator Dom/new.js
+++ Assignment 3 Calculator Dom/new.js
@@ -16,15 +16,15 @@ for (let i = 0; i < buttonsList.length; i++) {
 
 let CEButton = document.getElementById('CEButton')
 
-CEButton.addEventListener('click', ()=> {
-    if(screen.value){
+CEButton.addEventListener('click', () => {
+    if (screen.value) {
         screen.value = screen.value.slice(0, screen.value.length - 1);
     }
 })
 
 let facto = document.getElementById('fact')
 
-facto.addEventListener('click',()=> {
+facto.addEventListener('click', () => {
     let i, num, f;
     f = 1
     num = screen.value;
@@ -39,69 +39,69 @@ facto.addEventListener('click',()=> {
 
 let myAc = document.getElementById('acID')
 
-myAc.addEventListener('click', ()=>{
+myAc.addEventListener('click', () => {
     screen.value = ''
 })
 
 let newSin = document.getElementById('mySin')
 
-newSin.addEventListener('click', ()=> {
+newSin.addEventListener('click', () => {
     screen.value = Math.sin(screen.value);
 })
 
 let newCos = document.getElementById('myCos')
 
-newCos.addEventListener('click', ()=> {
+newCos.addEventListener('click', () => {
     screen.value = Math.cos(screen.value);
 })
 
 let newTan = document.getElementById('myTan')
 
-newTan.addEventListener('click', ()=> {
+newTan.addEventListener('click', () => {
     screen.value = Math.tan(screen.value);
 })
 
 let newPow = document.getElementById('myPow')
 
-newPow.addEventListener('click', ()=> {
+newPow.addEventListener('click', () => {
     screen.value = Math.pow(screen.value, 2);
 })
 
 let newSqrt = document.getElementById('sqrt')
 
-newSqrt.addEventListener('click', ()=> {
+newSqrt.addEventListener('click', () => {
     screen.value = Math.sqrt(screen.value, 2);
 })
 
 let newLog = document.getElementById('myLog')
 
-newLog.addEventListener('click', ()=> {
+newLog.addEventListener('click', () => {
     screen.value = Math.log(screen.value);
 })
 
 let newPi = document.getElementById('myPi')
 
-newPi.addEventListener('click', ()=> {
+newPi.addEventListener('click', () => {
     screen.value = 3.14159265359;
 })
 
 let newRad = document.getElementById('myRad')
 
-newRad.addEventListener('click', ()=> {
+newRad.addEventListener('click', () => {
     screen.value = 57.2958;
 })
 
 let newEquals = document.getElementById('myEquals')
 
-newEquals.addEventListener('click', ()=>{
+newEquals.addEventListener('click', () => {
     screen.value = eval(screen.value)
 })
 
 let newPercent = document.getElementById('percent')
 
-newPercent.addEventListener('click', ()=>{
-  Array.from(screen.value)
-  screen.value = (screen.value.split('*')[0]) * (screen.value.split('*')[1])/100;
+newPercent.addEventListener('click', () => {
+    Array.from(screen.value)
+    screen.value = (screen.value.split('*')[0]) * (screen.value.split('*')[1]) / 100;
 })
 
 
